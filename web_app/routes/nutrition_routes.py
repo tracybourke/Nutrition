c64936d90cc96a62513a42e40d6f20602095f629
tracybourke <77201812+tracybourke@users.noreply.github.com>
2023-08-12 08:11:16 -0500

    changed from unemployment to nutrition

--- web_app/routes/nutrition_routes.py
+++ web_app/routes/nutrition_routes.py
@@ -4,23 +4,23 @@ this is the "web_app/routes/nutrition_routes.py" file...
 
 from flask import Blueprint, request, render_template, redirect, flash
 
-from app.edanam import fetch_unemployment_data, format_pct
+from app.edanam import fetch_nutrtion_data, format_pct
 
-unemployment_routes = Blueprint("unemployment_routes", __name__)
+nutrition_routes = Blueprint("nutrition_routes", __name__)
 
 
-@unemployment_routes.route("/unemployment/dashboard")
-def unemployment_dashboard():
-    print("UNEMPLOYMENT DASHBOARD...")
+@nutrition_routes.route("/nutrition/dashboard")
+def nutrition_dashboard():
+    print("NUTRITION DASHBOARD...")
 
     try:
-        data = fetch_unemployment_data()
+        data = fetch_nutrition_data()
         latest = data[0]
         latest_rate_pct = format_pct(float(latest["value"]))
         latest_date = latest["date"]
 
         #flash("Fetched Latest Unemployment Data!", "success")
-        return render_template("unemployment_dashboard.html",
+        return render_template("nutrition_dashboard.html",
             latest_rate_pct=latest_rate_pct,
             latest_date=latest_date,
             data=data
@@ -28,20 +28,20 @@ def unemployment_dashboard():
     except Exception as err:
         print('OOPS', err)
 
-        #flash("Unemployment Data Error. Please try again!", "danger")
+        #flash("Data Error. Please try again!", "danger")
         return redirect("/")
 
 #
 # API ROUTES
 #
 
-@unemployment_routes.route("/api/unemployment.json")
-def unemployment_api():
-    print("UNEMPLOYMENT DATA (API)...")
+@nutrition_routes.route("/api/edanam.json")
+def edanam_api():
+    print("NUTRITION DATA (API)...")
 
     try:
-        data = fetch_unemployment_data()
+        data = fetch_nutrition_data()
         return data
     except Exception as err:
         print('OOPS', err)
-        return {"message":"Unemployment Data Error. Please try again."}, 404
+        return {"message":"Data Error. Please try again."}, 404
